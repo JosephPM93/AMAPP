70e1f0255b15782598e74adff37895dd83b5631e
IrvingMachado <u20201205@univo.edu.sv>
2021-11-18 18:56:07 -0600

    Agregando metodos CRUD para modelo Dosis

--- src/main/java/Controladores/CoreCRUDControlador.java
+++ src/main/java/Controladores/CoreCRUDControlador.java
@@ -5,10 +5,72 @@
  */
 package Controladores;
 
+//Importancion de librerias
+import Modelos.Conexion;
+import Modelos.Dosis;
+import Modelos.Vacuna;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author José Padilla
  */
 public class CoreCRUDControlador {
-    
+    //Metodos CRUD por cada modelo
+
+    //Creacion de objecto con la conexion a DB
+    private Conexion con = new Conexion();
+
+    //Creacion de metodo select dosis
+    public List<Dosis> SelectDosis() {
+        con.getSession().beginTransaction();
+        List<Dosis> ListaDatos = con.getSession().createQuery("from dosis").getResultList();
+        return ListaDatos;
+    }
+
+    //Creacion de metodo Insert dosis
+    public boolean InsertDosis(Dosis dato) {
+        try {
+            con.getSession().beginTransaction();
+            con.getSession().save(dato);
+            con.getSession().getTransaction().commit();
+            con.getSession().close();
+
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //Creacion de metodo Delete dosis
+    public boolean DeleteDosis(Dosis dato) {
+        try {
+            con.getSession().beginTransaction();
+            con.getSession().delete(dato);
+            con.getSession().getTransaction().commit();
+            con.getSession().close();
+
+            return true;
+
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //Creacion de metodo Update dosis
+    public boolean UpdateDosis(Dosis dato) {
+        try {
+            con.getSession().beginTransaction();
+            con.getSession().update(dato);
+            con.getSession().getTransaction().commit();
+            con.getSession().close();
+
+            return true;
+        
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
