6dde2d572f131befacb0bfbe09e28fec9016dac6
IrvingMachado <u20201205@univo.edu.sv>
2021-11-28 21:48:06 -0600

    Agregacion de comentarios Java doc en clase ConsultasControlador

--- src/main/java/Controladores/ConsultasControlador.java
+++ src/main/java/Controladores/ConsultasControlador.java
@@ -12,19 +12,27 @@ import javax.swing.JOptionPane;
  * @author José Padilla
  */
 public class ConsultasControlador {
-
+    /**
+     * Metodo para mostrar que los datos han sido guardados con exito
+     **/
     public void DatosGuardadosMensaje() {
         JOptionPane.showMessageDialog(null, "Datos guardados correctamente", "Ok", JOptionPane.INFORMATION_MESSAGE);
     }
-
+    /**
+     * Metodo para mostrar que hubo un problema con el almacenamiento de los datos
+     **/
     public void MensajeError() {
         JOptionPane.showMessageDialog(null, "¡Hubo un problema!", "Error", JOptionPane.ERROR_MESSAGE);
     }
-
+    /**
+     * Metodo para mostrar que el valor ingresado no es valido
+     **/
     public void ValorNoValidoMensaje() {
         JOptionPane.showMessageDialog(null, "¡Valor ingresado no válido!", "Error", JOptionPane.WARNING_MESSAGE);
     }
-    
+    /**
+     * Metodo para mostrar que no puede dejar una seleccion en blanco
+     **/
     public void NoHaySeleccionMensaje(){
         JOptionPane.showMessageDialog(null, "¡No se ha seleccionado nada!", "Error", JOptionPane.WARNING_MESSAGE);
     }
