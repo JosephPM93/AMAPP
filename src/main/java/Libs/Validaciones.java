36f632fce478484da4a3cd7394fa668daaecc175
IrvingMachado <u20201205@univo.edu.sv>
2021-12-05 12:07:10 -0600

    Comentarios Java Doc en packages Libs

--- src/main/java/Libs/Validaciones.java
+++ src/main/java/Libs/Validaciones.java
@@ -13,19 +13,33 @@ import javax.swing.JTextField;
  * @author José Padilla
  */
 public class Validaciones {
-
+    /**
+     * Metodo para validar si un JTextField esta vacio
+     * @return boolean 
+     **/
     public boolean EsVacio(JTextField jtf) {
         return jtf.getText().trim().equals("");
     }
-    
+    /**
+     * Metodo para validar si un JDateChooser esta vacio
+     * @return boolean 
+     **/
     public boolean EsVacio(JDateChooser jdc) {
         return jdc.getDate() == null;
     }
-    
+    /**
+     * Metodo para validar si un String esta vacio
+     * @param text
+     * @return boolean 
+     **/
     public boolean EsVacio(String text) {
         return text == null || text.trim().equals("");
     }
-
+    /**
+     * Metodo para validar un numero decimal
+     * @param jtf
+     * @return boolean 
+     **/
     public boolean JTextField_EsNumeroDecimal(JTextField jtf) {
         try {
             double test = Double.parseDouble(jtf.getText().trim());
